21faa77c577596083de86a569c8fcdc018a7bed9
codeico <bangico2002@gmail.com>
2025-04-20 03:36:05 +0700

    Initial commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,23 +4,21 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'My Docs',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
-	],
+  site: 'https://docs.bangcode.id',
+  integrations: [
+    starlight({
+      customCss: ['/src/styles/global.css'],  // Menyediakan path dalam array
+      title: 'BANGCODE',
+      social: [
+        { icon: 'github', label: 'GitHub', href: 'https://github.com/codeico' }
+      ],
+      sidebar: [
+        {
+          label: '0G Labs',
+          // Autogenerate a group of links for the 'constellations' directory.
+          autogenerate: { directory: '0g-labs' },
+        },
+      ],
+    }),
+  ],
 });
