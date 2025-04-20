a02190f966daea711719353f0f3dcdfe92c07a8c
codeico <bangico2002@gmail.com>
2025-04-20 19:37:08 +0700

    Update Konten

--- astro.config.mjs
+++ astro.config.mjs
@@ -14,11 +14,29 @@ export default defineConfig({
         { icon: 'github', label: 'GitHub', href: 'https://github.com/codeico' }
       ],
       sidebar: [
-        {
-          label: '0G Labs',
-          // Autogenerate a group of links for the 'constellations' directory.
-          autogenerate: { directory: '0g-labs' },
-        },
+		{
+			label: '0G Labs',
+			items: [
+			  '0g-labs',
+			  {
+				label: 'Validator Nodes',
+				items: [
+				  '0g-labs/validator-node',
+				  '0g-labs/validator-node/backup',
+				  '0g-labs/validator-node/commands',
+				  '0g-labs/validator-node/addrbook',
+				  '0g-labs/validator-node/livepeers',
+				],
+			  },
+			  {
+				label: 'Storage Node',
+				items: [
+				  '0g-labs/storage-node/installation',
+				  '0g-labs/storage-node/snapshot',
+				],
+			  },
+			],
+		},
       ],
     }),
   ],
